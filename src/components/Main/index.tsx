bf6aa07a4b44304e22280264c0906722bfe1e856
caduxl007 <eduardo2020dev@gmail.com>
2021-03-08 16:18:40 -0300

    Initial commit from Create Next App

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,15 @@
+import * as S from './styles'
+
+const Main = () => (
+  <S.Wrapper>
+    <S.Title>React Avançado</S.Title>
+    <S.Description>
+      TypeScript, ReactJS, NextJS e Styled Components
+    </S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="Um desenvolvedor no computador"
+    />
+  </S.Wrapper>
+)
+export default Main
