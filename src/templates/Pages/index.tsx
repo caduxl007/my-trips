8e694a5025870044b5e5ced74ccfb168613f96dc
caduxl007 <eduardo2020dev@gmail.com>
2021-03-09 18:59:40 -0300

    next-seo

--- src/templates/Pages/index.tsx
+++ src/templates/Pages/index.tsx
@@ -1,3 +1,4 @@
+import { NextSeo } from 'next-seo'
 import { CloseOutline } from '@styled-icons/evaicons-outline'
 import LinkWrapper from 'components/LinkWrapper'
 
@@ -9,17 +10,31 @@ export type PageTemplateProps = {
 }
 
 const PageTemplate = ({ heading, body }: PageTemplateProps) => (
-  <S.Content>
-    <LinkWrapper href="/">
-      <CloseOutline size={32} />
-    </LinkWrapper>
+  <>
+    <NextSeo
+      title="My Trips - About"
+      description="A simple project to show in a map the places that I went and show more informations and photos when clicked."
+      canonical="https://my-trips.com.br"
+      openGraph={{
+        url: 'https://my-trips.com.br',
+        title: 'My Trips',
+        description:
+          'A simple project to show in a map the places that I went and show more informations and photos when clicked.',
+        site_name: 'My Trips'
+      }}
+    />
+    <S.Content>
+      <LinkWrapper href="/">
+        <CloseOutline size={32} />
+      </LinkWrapper>
 
-    <S.Heading>{heading}</S.Heading>
+      <S.Heading>{heading}</S.Heading>
 
-    <S.Body>
-      <div dangerouslySetInnerHTML={{ __html: body }}></div>
-    </S.Body>
-  </S.Content>
+      <S.Body>
+        <div dangerouslySetInnerHTML={{ __html: body }}></div>
+      </S.Body>
+    </S.Content>
+  </>
 )
 
 export default PageTemplate
