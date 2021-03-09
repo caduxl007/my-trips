cacba10660d50edb31a39f00817afc93876bf3c1
caduxl007 <eduardo2020dev@gmail.com>
2021-03-09 18:54:15 -0300

    styles about

--- src/templates/Pages/styles.ts
+++ src/templates/Pages/styles.ts
@@ -3,12 +3,17 @@ import styled from 'styled-components'
 export const Content = styled.section`
   text-align: center;
   display: flex;
-  align-items: center;
   justify-content: center;
-  flex-direction: column;
+  align-items: center;
   height: 100vh;
+  flex-direction: column;
   max-width: var(--container);
-  margin: auto;
+  margin: 0 auto;
+  padding: 0 20px;
+
+  @media (max-width: 490px) {
+    display: block;
+  }
 `
 
 export const Heading = styled.h1`
@@ -18,7 +23,8 @@ export const Heading = styled.h1`
 
 export const Body = styled.div`
   p {
-    font-size: var(--medium);
+    font-size: 1.8rem;
     line-height: var(--medium);
   }
+  padding-bottom: 30px;
 `
