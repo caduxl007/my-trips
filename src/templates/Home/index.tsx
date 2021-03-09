0062bf6d1546fe2a7b9f23d63513b29cba67dcc8
caduxl007 <eduardo2020dev@gmail.com>
2021-03-09 18:16:16 -0300

    mapbox, next-seo and isr

---
+++ src/templates/Home/index.tsx
@@ -0,0 +1,32 @@
+import { NextSeo } from 'next-seo'
+
+import dynamic from 'next/dynamic'
+import { InfoOutline } from '@styled-icons/evaicons-outline'
+
+import LinkWrapper from 'components/LinkWrapper'
+import { MapProps } from 'components/Map'
+
+const Map = dynamic(() => import('components/Map'), { ssr: false })
+
+export default function HomeTemplate({ places }: MapProps) {
+  return (
+    <>
+      <NextSeo
+        title="My Trips"
+        description="A simple project to show in a map the places that I went and show more informations and photos when clicked."
+        canonical="https://my-trips.com.br"
+        openGraph={{
+          url: 'https://my-trips.com.br',
+          title: 'My Trips',
+          description:
+            'A simple project to show in a map the places that I went and show more informations and photos when clicked.',
+          site_name: 'My Trips'
+        }}
+      />
+      <LinkWrapper href="/about">
+        <InfoOutline size={32} />
+      </LinkWrapper>
+      <Map places={places} />
+    </>
+  )
+}
